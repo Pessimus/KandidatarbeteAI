5e47c69bc8f2ce896dca1751d51fb274844f2b2d
Fooughhy <foughman.tobias@gmail.com>
2016-02-26 14:44:10 +0100

    Added controller class+package.
    It implements PropertyChangeListener to be able to listen to the View.

---
+++ src/Controller/Controller.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * Created by Tobias on 2016-02-26.
+ */
+public class Controller implements PropertyChangeListener {
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+
+	}
+}
