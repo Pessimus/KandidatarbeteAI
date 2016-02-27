a956afd8fbdc31ef00745fb7015a24b373e1dcbc
Fooughhy <foughman.tobias@gmail.com>
2016-02-27 19:37:20 +0100

    Implementing methods and PropertyChangeListener interface
    'propertyChange' method is unfinished since it's unknown how to,
    exactly, handle the events.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,34 +1,48 @@
 package Controller;
 
-//import Model.IModel;
-//import Model.World;
-//import View.IView;
+import Model.World;
+import View.View;
 
+import javafx.util.Pair;
+import org.lwjgl.input.Keyboard;
+
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
 
 /**
  * Created by Tobias on 2016-02-26.
  */
 public class Controller implements PropertyChangeListener {
-	//IModel gameModel;
-	//IView gameView;
+	private World gameModel;
+	private View gameView;
+
+	private final Queue<Integer> keyboardInputQueue;
+	private final Queue<Integer> mouseInputQueue;
+
+	private final Semaphore keyboardSema = new Semaphore(1);
+	private final Semaphore mouseSema = new Semaphore(1);
 
 	public static void main(String[] args){
-		//IModel model = new World();
-		//IView view = new View();
-		//new Controller();
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		World model = new World(d.getWidth(), d.getHeight());
+		View view = new View();
+		new Controller(view, model);
 	}
 
-	/*
-	public Controller(IView view, IModel model){
+	public Controller(View view, World model){
+		keyboardInputQueue = new LinkedList<>();
+		mouseInputQueue = new LinkedList<>();
 		setView(view);
 		setModel(model);
 	}
-	*/
 
-	/*
-	public boolean setView(IView view){
+	public boolean setView(View view){
 		if(view != null){
 			gameView = view;
 			return true;
@@ -37,7 +51,7 @@ public class Controller implements PropertyChangeListener {
 		return false;
 	}
 
-	public boolean setModel(IModel model){
+	public boolean setModel(World model){
 		if(model != null){
 			gameModel = model;
 			return true;
@@ -45,10 +59,58 @@ public class Controller implements PropertyChangeListener {
 
 		return false;
 	}
-	*/
+
+	private void updateView(){
+
+	}
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
+		if(evt != null){
+			if(!evt.getPropertyName().equals(null)){
+				if(evt.getSource().equals(gameView)){
+					// If the source of the event is the 'View', handle that separately.
+					if(evt.getPropertyName().equals("keyboard")) {
+						// If the 'View' is sending 'Keyboard'-inputs, put them in the correct queue.
+						try {
+							keyboardSema.acquire();
+							keyboardInputQueue.offer((Integer) evt.getNewValue());
+							keyboardSema.release();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					} else if(evt.getPropertyName().equals("mouse")){
+						// If the View is sending 'Mouse'-inputs, put them in the correct queue.
+						try{
+							mouseSema.acquire();
+							mouseInputQueue.offer((Integer) evt.getNewValue());
+							mouseSema.release();
+						} catch (InterruptedException e){
+							e.printStackTrace();
+						}
+					}
+				}
 
+				if(evt.getSource().equals(gameModel)){
+					// If the source of the event is the 'Model', handle that separately.
+					if(evt.getPropertyName().equals("input")) {
+						// If the 'Model' is asking for inputs, provide that.
+						try {
+							keyboardSema.acquire();
+							Integer[] keyInts = (Integer[])keyboardInputQueue.toArray();
+							keyboardSema.release();
+
+							mouseSema.acquire();
+							Integer[] mouseInts = (Integer[])mouseInputQueue.toArray();
+							mouseSema.release();
+
+							//gameModel.provideInputs(keyInts, mouseInts); TODO: Implement a method in World that receives lists of integers.
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
 	}
 }
