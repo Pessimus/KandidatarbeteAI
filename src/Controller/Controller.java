aedf9d7a89aeae5a4db69c565fa4fd2db8255762
Fooughhy <foughman.tobias@gmail.com>
2016-02-29 12:26:36 +0100

    Further implementation of Controller class. View had to be temporarily implemented.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -3,10 +3,12 @@ package Controller;
 import Model.World;
 import View.View;
 
-import java.awt.*;
+import java.awt.Toolkit;
+import java.awt.Dimension;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
 
@@ -17,16 +19,21 @@ public class Controller implements PropertyChangeListener {
 	private World gameModel;
 	private View gameView;
 
-	private final Queue<Integer> keyboardInputQueue;
-	private final Queue<Integer> mouseInputQueue;
+	private final Queue<Integer[]> keyboardInputQueue;
+	private final Queue<Integer[]> mouseInputQueue;
 
 	private final Semaphore keyboardSema = new Semaphore(1);
 	private final Semaphore mouseSema = new Semaphore(1);
 
+	// TODO: Remove this enum!
+	public enum objects{
+		CHARACTER,STRUCTURE,TERRAIN;
+	}
+
 	public static void main(String[] args){
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		World model = new World(d.getWidth(), d.getHeight());
-		View view = new View();
+		View view = new View("Kandidat");
 		new Controller(view, model);
 	}
 
@@ -40,7 +47,7 @@ public class Controller implements PropertyChangeListener {
 	public boolean setView(View view){
 		if(view != null){
 			gameView = view;
-			//gameView.addPropertyListener(this); // TODO: 'View' should use PropertyChangeSupport
+			//gameView.addPropertyChangeListener(this); // TODO: 'View' should use PropertyChangeSupport
 			return true;
 		}
 
@@ -50,7 +57,7 @@ public class Controller implements PropertyChangeListener {
 	public boolean setModel(World model){
 		if(model != null){
 			gameModel = model;
-			//gameModel.addPropertyListener(this); // TODO: 'Model' should use PropertyChangeSupport
+			//gameModel.addPropertyChangeListener(this); // TODO: 'Model' should use PropertyChangeSupport
 			return true;
 		}
 
@@ -58,7 +65,65 @@ public class Controller implements PropertyChangeListener {
 	}
 
 	private void updateView(){
+		//List<Enum> objectList = gameModel.getViewableObjects();
+		// getViewableObjects() is expected to return a 'List' of 3 arrays:
+		// An array with all enum values for the objects;
+		// An array with the x-coordinates for these objects
+		// An array with the y-coordinates for these objects
+		//gameView.drawObjects(objectList);
+	}
 
+	private void updateModel(){
+
+	}
+
+	private void handleViewEvent(PropertyChangeEvent evt){
+		if(evt.getPropertyName().equals("keyboard")) {
+			// If the 'View' is sending 'Keyboard'-inputs, put them in the correct queue.
+			try {
+				Integer[] newValue = (Integer[]) evt.getNewValue();
+				keyboardSema.acquire();
+				keyboardInputQueue.offer(newValue);
+				keyboardSema.release();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else if(evt.getPropertyName().equals("mouse")){
+			// If the View is sending 'Mouse'-inputs, put them in the correct queue.
+			try{
+				Integer[] newValue = (Integer[]) evt.getNewValue();
+				mouseSema.acquire();
+				mouseInputQueue.offer(newValue);
+				mouseSema.release();
+			} catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void handleModelEvent(PropertyChangeEvent evt){
+		if(evt.getPropertyName().equals("getInput")) {
+			// If the 'Model' is asking for inputs, provide that.
+			try {
+				keyboardSema.acquire();
+				Object[] tempList = keyboardInputQueue.toArray();
+				keyboardSema.release();
+
+				Integer[] keyInts = (Integer[])tempList;
+
+				mouseSema.acquire();
+				tempList = mouseInputQueue.toArray();
+				mouseSema.release();
+				Integer[] mouseInts = (Integer[])tempList;
+
+				//gameModel.provideInputs(keyInts, mouseInts); TODO: Implement a method in World that receives lists of integers.
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else if(evt.getPropertyName().equals("update")){
+			// Get coordinates of all objects in the viewable area!
+			updateView();
+		}
 	}
 
 	@Override
@@ -67,45 +132,12 @@ public class Controller implements PropertyChangeListener {
 			if(!evt.getPropertyName().equals(null)){
 				if(evt.getSource().equals(gameView)){
 					// If the source of the event is the 'View', handle that separately.
-					if(evt.getPropertyName().equals("keyboard")) {
-						// If the 'View' is sending 'Keyboard'-inputs, put them in the correct queue.
-						try {
-							keyboardSema.acquire();
-							keyboardInputQueue.offer((Integer) evt.getNewValue());
-							keyboardSema.release();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					} else if(evt.getPropertyName().equals("mouse")){
-						// If the View is sending 'Mouse'-inputs, put them in the correct queue.
-						try{
-							mouseSema.acquire();
-							mouseInputQueue.offer((Integer) evt.getNewValue());
-							mouseSema.release();
-						} catch (InterruptedException e){
-							e.printStackTrace();
-						}
-					}
+					handleViewEvent(evt);
 				}
 
 				if(evt.getSource().equals(gameModel)){
 					// If the source of the event is the 'Model', handle that separately.
-					if(evt.getPropertyName().equals("input")) {
-						// If the 'Model' is asking for inputs, provide that.
-						try {
-							keyboardSema.acquire();
-							Integer[] keyInts = (Integer[])keyboardInputQueue.toArray();
-							keyboardSema.release();
-
-							mouseSema.acquire();
-							Integer[] mouseInts = (Integer[])mouseInputQueue.toArray();
-							mouseSema.release();
-
-							//gameModel.provideInputs(keyInts, mouseInts); TODO: Implement a method in World that receives lists of integers.
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
+					handleModelEvent(evt);
 				}
 			}
 		}
--- src/View/View.java
+++ src/View/View.java
@@ -1,7 +1,33 @@
 package View;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 /**
  * Created by Oskar on 2016-02-26.
  */
-public class View {
+// TODO: Discard this class when merging into 'master'!
+public class View extends StateBasedGame{
+	private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+	public View(String name){
+		super(name);
+	}
+
+	@Override
+	public void initStatesList(GameContainer gameContainer) throws SlickException {
+
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		this.pcs.addPropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		this.pcs.removePropertyChangeListener(listener);
+	}
 }
