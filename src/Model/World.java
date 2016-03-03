a75f6207d8cc3cac67722dc5ffbb427f7226582f
Martin <kjell.martin.nilsson@gmail.com>
2016-03-03 09:17:16 +0100

    Added a method for sending data from model to controler

--- src/Model/World.java
+++ src/Model/World.java
@@ -11,19 +11,19 @@ public class World {
 	private HashMap<Integer,Character> characters;
 	private LinkedList<ICollidable> collidables; //TODO custom list supporting buublesort and incrtionsort
 	private LinkedList<ITimeable> timables; //TODO custom list supporting buublesort and incrtionsort
-	private double with;
+	private double width;
 	private double height;
 
-	public World (double with, double height){
-		this.with = with;
+	public World (double width, double height){
+		this.width = width;
 		this.height = height;
 		this.collidables = new LinkedList<ICollidable>();
 		this.timables = new LinkedList<ITimeable>();
 		this.characters = new HashMap<>();
 	}
 
-	public static World generateWorld(double with, double height, int nbrOfResources) {
-		World world = new World(with,height);
+	public static World generateWorld(double width, double height, int nbrOfResources) {
+		World world = new World(width,height);
 
 		return null;
 	}
@@ -40,12 +40,18 @@ public class World {
 		return c;
 	}
 
-	private boolean addCollidable(double xPoss, double yPoss, double radius){
+	private boolean addCollidable(double xPoss, double yPoss, double radius){return false;}
 
-
-		return false;
-
+	public LinkedList<Vissible> getVisible(){
+		LinkedList<Vissible> vissibles = new LinkedList<>();
+		for(ICollidable visible : collidables){
+			Vissible tmp = new Vissible();
+			tmp.xPoss = visible.getX();
+			tmp.yPoss = visible.getY();
+			tmp.radius = visible.getRadius();
+			vissibles.add(tmp);
+		}
+		return vissibles;
 	}
-
 
 }
