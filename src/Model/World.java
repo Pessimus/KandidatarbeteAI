38b5e13f793a0e1d92f5cfcd16b2abe593279bab
Martin <kjell.martin.nilsson@gmail.com>
2016-03-03 08:10:47 +0100

    Some work om world

--- src/Model/World.java
+++ src/Model/World.java
@@ -8,24 +8,27 @@ import java.util.LinkedList;
 public class World {
 
 	private LinkedList<ICollidable> collidables; //TODO custom list supporting buublesort and incrtionsort
+	private LinkedList<ITimable> timables; //TODO custom list supporting buublesort and incrtionsort
 	private double with;
 	private double height;
 
 	public World (double with, double height){
+		this.with = with;
+		this.height = height;
 		this.collidables = new LinkedList<ICollidable>();
+		this.timables = new LinkedList<ITimable>();
 
 	}
 
 	public static World generateWorld(double with, double height, int nbrOfResources) {
 		World world = new World(with,height);
 
-
-
 		return null;
-
 	}
 
 	private boolean addCollidable(double xPoss, double yPoss, double radius){
+
+
 
 		return false;
 
