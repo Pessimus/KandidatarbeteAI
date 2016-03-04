f8ce9625c9512dd30b802fd531d4033eea01e817
Victor Gustafsson <victorgustafsson@hotmail.com>
2016-03-04 13:24:41 +0100

    Implementerat basic needs. updateNeeds(), secondaryNeeds, checkar om karaktären är död

--- src/Model/ITimeable.java
+++ src/Model/ITimeable.java
@@ -4,9 +4,5 @@ package Model;
  * Created by Tobias on 2016-02-26.
  */
 public interface ITimeable {
-	/*double getTimeInterval();
-	void setTimeInterval(double period);
-	void onUpdate();*/
 	public void update();
-
 }
