6dd48dda3385cac08632864f5a5ff1e63efa3a01
Martin <kjell.martin.nilsson@gmail.com>
2016-03-09 10:28:07 +0100

    work on collision

--- src/Model/CollisionList.java
+++ src/Model/CollisionList.java
@@ -5,6 +5,9 @@ package Model;
  */
 public class CollisionList {
 
+	private Node currentNode;
+	private Node startNode;
+
 	public double getX() {return currentNode.getValue().getX();}
 	public double getY() {return currentNode.getValue().getY();}
 	public double getRadius() {return currentNode.getValue().getCollisionRadius();}
@@ -12,7 +15,82 @@ public class CollisionList {
 	public boolean next() {if (currentNode.getNext() != null) {currentNode = currentNode.getNext(); return true;} else {return false;}}
 	public boolean previous() {if (currentNode.getPrevious() != null) {currentNode = currentNode.getPrevious(); return true;} else {return false;}}
 
-	private Node currentNode;
+
+
+
+	public CollisionList(){
+		this.startNode = new Node(new ICollidable() {
+			@Override
+			public double getX() {
+				return -10000;
+			}
+
+			@Override
+			public double getY() {
+				return -10000;
+			}
+
+			@Override
+			public double getCollisionRadius() {
+				return -10000;
+			}
+		}, null, null);
+		this.currentNode = this.startNode;
+
+	}
+
+	//public CollissionItterator Iterator(){}
+
+	public void sort(){
+		int itter = 0;
+		if(startNode.next == null){
+			return;
+		}
+		boolean hasChanged = true;
+		Node sortNode;
+		while(hasChanged) {
+			sortNode = startNode.next.next;
+			hasChanged = false;
+			while (sortNode != null) {
+				if(sortNode.value.getY() < sortNode.previous.value.getY()){
+					swap(sortNode.previous, sortNode);
+					hasChanged = true;
+				}
+				//System.out.println("............"+sortNode.value.getY());
+				//System.out.println("////////////"+sortNode.next.value.getY());
+				sortNode = sortNode.next;
+			}
+			//System.out.println("----");
+			//this.print();
+			itter++;
+		}
+		System.out.println("itter = " + itter);
+	}
+
+	public void add(ICollidable addValue){
+		this.currentNode.next = new Node(addValue, currentNode,null);
+		this.currentNode = this.currentNode.next;
+	}
+
+	private void swap(Node previous, Node next) {
+		next.previous = previous.previous;
+		previous.next = next.next;
+
+		next.next=previous;
+		previous.previous=next;
+
+		next.previous.next = next;
+		previous.next.previous = previous;
+	}
+
+	public void print(){
+		Node tmp = startNode.next;
+
+		while(tmp != null){
+			System.out.println(tmp.value.getY());
+			tmp = tmp.next;
+		}
+	}
 
 	private class Node{
 
