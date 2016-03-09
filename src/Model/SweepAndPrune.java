30fa6fdc353b179dba348290d3ee191f128fefc3
Martin <kjell.martin.nilsson@gmail.com>
2016-03-09 12:00:59 +0100

    Work on the SweepAndPrune class

--- src/Model/SweepAndPrune.java
+++ src/Model/SweepAndPrune.java
@@ -5,6 +5,27 @@ package Model;
  */
 public class SweepAndPrune {
 
+	private CollisionList xList;
+	private CollisionList yList;
 
+	public SweepAndPrune(){
+		xList = new CollisionList(CollisionList.Axis.X);
+		xList = new CollisionList(CollisionList.Axis.Y);
+	}
+
+	public void sort(){
+		this.xList.sort();
+		this.yList.sort();
+	}
+
+	public void add(ICollidable collidable){
+		this.xList.add(collidable);
+		this.yList.add(collidable);
+	}
+
+	public void remove(ICollidable collidable){
+		this.xList.remove(collidable);
+		this.yList.remove(collidable);
+	}
 
 }
