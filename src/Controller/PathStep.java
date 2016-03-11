c56519163e2996dbd006118f8398322e40a6441e
Gustav Grund Pihlgren <guspih@student.chalmers.se>
2016-03-11 13:30:21 +0100

    Revert "Revert "Completed PathStep""
    This reverts commit cbbe00abfc54d5ece52a3d054f2b72ba18323b57.

--- src/Controller/PathStep.java
+++ src/Controller/PathStep.java
@@ -25,23 +25,40 @@ public class PathStep {
         return Math.abs(nowx - nodex) <= reached_distance && Math.abs(nowy - nodey) <= reached_distance;
     }
 
-    // steps the character towards the node (or maybe returns the direction to move towards?)
-    public void stepTowards (double nowx, double nowy, Character c) {
-        double diffx = nodex - nowx;
-        double diffy = nodey - nowy;
+    // returns whether or not the node is within the given range
+    public boolean reached (double nowx, double nowy, double range) {
+        return Math.abs(nowx - nodex) <= range && Math.abs(nowy - nodey) <= range;
+    }
+
+    // steps the given character towards the node and returns true if they reached the node
+    public boolean stepTowards (Character c) {
+        double diffx = nodex - c.getX();
+        double diffy = nodey - c.getY();
 
         if (diffx > 0) {
             //if the character should move right
+            c.walkRight();
         } else if (diffx < 0) {
             //if the character should move left
+            c.walkRight();
         }
 
         if (diffy > 0) {
             //if the character should move down
+            c.walkDown();
         } else if (diffy < 0) {
             //if the character should move left
+            c.walkUp();
         }
 
+        return reached(c.getX(), c.getY(), c.getCollisionRadius());
     }
 
+    // returns the direction (in degrees) from the given coordinates towards this node (not implemented)
+    public double direction (double x, double y) {
+        double diffx = nodex - x;
+        double diffy = nodey - y;
+        double tangent diffy/diffx =
+        return 0;
+    }
 }
