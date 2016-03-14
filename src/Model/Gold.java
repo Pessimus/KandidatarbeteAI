c5dc15b88702b20e4ec7727e52b777e4b4d32736
Oskar Olsson <oskarol@student.chalmers.se>
2016-03-14 14:39:17 +0100

    Code Gold,meat,crops,fish

--- src/Model/Gold.java
+++ src/Model/Gold.java
@@ -3,5 +3,38 @@ package Model;
 /**
  * Created by Oskar on 2016-03-10.
  */
-public class Gold {
+public class Gold extends FiniteResource {
+
+    public static final ResourceType resourceType = ResourceType.GOLD;
+    public static final String resourceName = "Gold";
+
+    public Gold(int initial){
+        super(initial);
+    }
+    @Override
+    public ItemFactory getItemFactory() {
+        return null;
+    }
+
+    @Override
+    public IItem gatherResource() {
+        int resourceLeft = getResourcesLeft();
+        if(resourceLeft>0){
+            setResourcesLeft(resourceLeft-1);
+            return  null;
+        }
+        else{
+            return null;
+        }
+    }
+
+    @Override
+    public ResourceType getResourceType() {
+        return resourceType;
+    }
+
+    @Override
+    public String getResourceName() {
+        return resourceName;
+    }
 }
