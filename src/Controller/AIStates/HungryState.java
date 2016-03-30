ec681c28b2906a161db39d5a87af60a654aa1605
Victor Gustafsson <victorgustafsson@hotmail.com>
2016-03-30 13:46:32 +0200

    Wrote methods for HungryState

--- src/Controller/AIStates/HungryState.java
+++ src/Controller/AIStates/HungryState.java
@@ -3,9 +3,13 @@ package Controller.AIStates;
 import Controller.AbstractBrain;
 import Controller.ArtificialBrain;
 import Controller.CharacterAction;
+import Controller.PathStep;
 import Model.ICharacterHandle;
+import Model.IItem;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -14,6 +18,7 @@ import java.util.Queue;
 public class HungryState implements IState {
 	private ICharacterHandle body;
 	private final AbstractBrain brain;
+	private List<PathStep> pathToResource;
 
 	private Queue<CharacterAction> actionQueue = new LinkedList<>();
 
@@ -24,6 +29,38 @@ public class HungryState implements IState {
 	@Override
 	public void run() {
 		if(actionQueue.isEmpty()){
+		}
+		if(pathToResource == null) {
+			Iterator<IItem> iterator = body.getInventory().iterator();
+			IItem best = null;
+			int hungerAmount = -1;
+
+			loop:while(iterator.hasNext()){
+				IItem current = iterator.next();
+				switch (current.getType()) {
+					case FISH_ITEM: //TODO: CHANGE FISH TO FOOD
+						best = current;
+					/*
+					if(best == null){
+						best = current;
+						thirstAmount = best.getOutcome().getThirst();
+					}
+					else if(best.getOutcome().getThirst() > thirstAmount){
+						best = current;
+						thirstAmount = best.getOutcome().getThirst();
+					}
+					*/
+						break loop;
+				}
+			}
+
+			if(best == null){
+				// TODO: Pathfinding to nearest/best food-resource
+			}
+			else{
+				brain.setState(brain.getEatState());
+			}
+
 		}
 	}
 }
