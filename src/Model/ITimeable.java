7fb5b7f139402449b4c1d6f51e9ca24d9ec1240c
Martin <kjell.martin.nilsson@gmail.com>
2016-03-31 23:53:02 +0200

    Added javadock to IResource, ITimeable and ItemFactory

--- src/Model/ITimeable.java
+++ src/Model/ITimeable.java
@@ -4,6 +4,14 @@ package Model;
  * Created by Tobias on 2016-02-26.
  */
 public interface ITimeable {
+	/**
+	 * Updates the time dependant values.
+	 */
 	void updateTimeable();
+
+	/**
+	 * Checks if the object should be removed or not.
+	 * @return true if it should be removed, else false.
+	 */
 	boolean toBeRemoved();
 }
