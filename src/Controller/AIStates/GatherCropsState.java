d1721159281102a761ba1d3d4c210e5d9d47f31f
Fooughhy <foughman.tobias@gmail.com>
2016-03-31 14:34:29 +0200

    Added GatherCropsState and worked on HungryState

---
+++ src/Controller/AIStates/GatherCropsState.java
@@ -0,0 +1,34 @@
+package Controller.AIStates;
+
+import Controller.ArtificialBrain;
+import Model.ICollidable;
+import Model.RenderObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Tobias on 2016-03-31.
+ */
+public class GatherCropsState implements IState {
+	private final ArtificialBrain brain;
+
+	public GatherCropsState(ArtificialBrain b){
+		brain = b;
+	}
+
+	@Override
+	public void run() {
+		List<ICollidable> surround = brain.getBody().getInteractables();
+
+		for(ICollidable temp : surround){
+
+			// TODO: Don't use RENDER_OBJECT_ENUM, use Outcome in some way!!
+			if(temp.getRenderType().equals(RenderObject.RENDER_OBJECT_ENUM.CROPS)){
+			// TODO: Don't use RENDER_OBJECT_ENUM, use Outcome in some way!!
+
+				temp.interacted((Model.Character) brain.getBody());
+			}
+		}
+	}
+}
