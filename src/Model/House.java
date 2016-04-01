5eb47acd6195a87915a969d16bc50e845793e07c
Arklights <oskarol@student.chalmers.se>
2016-04-01 13:57:48 +0200

    Added functionality for structures

---
+++ src/Model/House.java
@@ -0,0 +1,24 @@
+package Model;
+
+/**
+ * Created by Oskar on 2016-04-01.
+ */
+public class House implements IStructure {
+
+//-----------------------------------------------VARIABLES------------------------------------------------------------\\
+    public static final StructureType structureType = StructureType.HOME;
+
+    private int capacity;
+    private int currentlyHousing;
+
+    //-----------------------------------------------CONSTRUCTOR------------------------------------------------------\\
+    public House(int capacity){this.capacity=capacity; }
+
+    @Override
+    public StructureType getStructureType() {
+        return structureType;
+    }
+
+    public int getCapacity
+}
+
--- src/Model/IStructure.java
+++ src/Model/IStructure.java
@@ -4,4 +4,13 @@ package Model;
  * Created by Oskar on 2016-04-01.
  */
 public interface IStructure {
+
+    enum StructureType{
+        HOME,
+        STOCKPILE
+    }
+
+    StructureType getStructureType();
+
+
 }
