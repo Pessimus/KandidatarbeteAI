c9fd842e73d761cb835c299661cd96dbab9eefd8
Arklights <oskarol@student.chalmers.se>
2016-04-01 15:15:29 +0200

    Added states for gathering fish and meat

---
+++ src/Controller/AIStates/GatherFishState.java
@@ -0,0 +1,7 @@
+package Controller.AIStates;
+
+/**
+ * Created by Oskar on 2016-04-01.
+ */
+public class GatherFishState {
+}
---
+++ src/Controller/AIStates/GatherMeatState.java
@@ -0,0 +1,37 @@
+package Controller.AIStates;
+
+import Controller.ArtificialBrain;
+import Model.Constants;
+import Model.ICollidable;
+import Toolkit.RenderObject;
+import com.sun.xml.internal.bind.v2.runtime.reflect.opt.Const;
+import org.lwjgl.Sys;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * Created by Oskar on 2016-04-01.
+ */
+public class GatherMeatState implements IState {
+    private final ArtificialBrain brain;
+
+    private int waitUpdates = 0;
+
+    public GatherMeatState(ArtificialBrain brain) {
+        this.brain = brain;
+    }
+
+    @Override
+    public void run() {
+        List<ICollidable> surrond = brain.getBody().getInteractables();
+
+        for(ICollidable temp : surrond){
+            // TODO: Don't use RENDER_OBJECT_ENUM, use Outcome in some way!!
+            if(temp.getRenderType().equals(RenderObject.RENDER_OBJECT_ENUM.LAKE)){
+            // TODO: Don't use RENDER_OBJECT_ENUM, use Outcome in some way!!
+
+            if(waitUpdates = (++waitUpdates % Constants.FISH_Ga))
+            }
+        }
+    }
+}
