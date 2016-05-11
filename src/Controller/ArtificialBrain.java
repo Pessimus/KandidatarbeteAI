e63167e7bdd6887f2c45f4500e71adad3a704113
Gustav Grund Pihlgren <guspih@student.chalmers.se>
2016-05-11 16:29:59 +0200

    Reverted previous change that caused nullPointerException

--- src/Controller/ArtificialBrain.java
+++ src/Controller/ArtificialBrain.java
@@ -355,7 +355,7 @@ public class ArtificialBrain implements AbstractBrain, PropertyChangeListener {
 	}
 
 	public void stackResourceToGather(ResourceTuple stackedResource) {
-		if (gatherStack.isEmpty() || (!gatherStack.peek().resourceType.equals(stackedResource.resourceType) || !(gatherStack.peek().resourceAmount == stackedResource.resourceAmount))) {gatherStack.push(stackedResource);}
+		gatherStack.push(stackedResource);
 	}
 
 	public IStructure.StructureType peekStructureStack() {
